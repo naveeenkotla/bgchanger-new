216703cd2c47996a8910bb38de6ef1ec7721bada
naveeen kotla <naveenkotla3232@gmail.com>
2023-09-29 22:10:11 +0530

    all the files are added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
 function App() {
+  let [color,setColor]=useState("olive")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+    <div className='flex justify-center flex-wrap '>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'red'}} onClick={()=>setColor("red")}>red</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'green'}} onClick={()=>setColor("green")}>green</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'blue'}} onClick={()=>setColor("blue")}>blue</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'YELLOW'}} onClick={()=>setColor("YELLOW")}>YELLOW</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'pink'}} onClick={()=>setColor("pink")}>pink</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'black'}} onClick={()=>setColor("black")}>black</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'white'}} onClick={()=>setColor("white")}>white</button>
+      <button className=' rounded-l p-4 m-2' style={{backgroundColor:'maroon'}} onClick={()=>setColor("maroon")}>maroon</button>
+
+
+    </div>
     </div>
   );
 }
